43e317303be417eab0c017f7a023265be5df39b0
Miebi <User@DESKTOP-1EK6ASL>
2024-07-12 21:10:53 +0100

    Chore: Displaying platform Icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,43 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import React from "react";
+import { IPlatform } from "../hooks/useGames";
+import { Text, Icon, HStack } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface IProp {
+  platforms: IPlatform[];
+}
+
+const PlatformIconList = ({ platforms }: IProp) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    android: FaAndroid,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useState, useEffect } from "react";
 import apiClient from "../components/services/api-client";
 import { CanceledError } from "axios";
 
+export interface IPlatform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface IGame {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: IPlatform }[];
 }
 
 interface IFetchGamesResponse {
