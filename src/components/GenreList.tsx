219cd0e24275f0f76653dbe07e746eb4368aa4b9
Miebi <User@DESKTOP-1EK6ASL>
2024-07-21 17:46:59 +0100

    Chore:Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import useGenres, { IGenre } from "../hooks/useGenres";
 import {
   Button,
+  Heading,
   HStack,
   Image,
   List,
@@ -21,29 +22,41 @@ const GenreList = ({ onSelectGenre, selectedGenre }: IProps) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <div style={{ height: "600px", overflowY: "scroll", overflowX: "hidden" }}>
-      <List>
-        {data.map((genre) => (
-          <ListItem key={genre.id} paddingY="5px">
-            <HStack>
-              <Image
-                boxSize="32px"
-                borderRadius={8}
-                src={getCroppedImageUrl(genre.image_background)}
-              />
-              <Button
-                fontSize="large"
-                variant="link"
-                onClick={() => onSelectGenre(genre)}
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-              >
-                {genre.name}
-              </Button>
-            </HStack>
-          </ListItem>
-        ))}
-      </List>
-    </div>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <div
+        style={{ height: "600px", overflowY: "scroll", overflowX: "hidden" }}
+      >
+        <List>
+          {data.map((genre) => (
+            <ListItem key={genre.id} paddingY="5px">
+              <HStack>
+                <Image
+                  objectFit="cover"
+                  boxSize="32px"
+                  borderRadius={8}
+                  src={getCroppedImageUrl(genre.image_background)}
+                />
+                <Button
+                  whiteSpace="normal"
+                  textAlign="left"
+                  fontSize="large"
+                  variant="link"
+                  onClick={() => onSelectGenre(genre)}
+                  fontWeight={
+                    genre.id === selectedGenre?.id ? "bold" : "normal"
+                  }
+                >
+                  {genre.name}
+                </Button>
+              </HStack>
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    </>
   );
 };
 
